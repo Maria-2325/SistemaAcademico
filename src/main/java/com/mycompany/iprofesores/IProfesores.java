3f57716c956f705474859b8af45fb816f2967a48
wsxdev <williamsxdev@gmail.com>
2025-06-29 03:35:08 -0500

    Refactor: mejora la implementación de las clases Decanos y Profesores, actualizando métodos para agregar, actualizar, buscar y eliminar elementos. También se añaden getters y setters en la clase Persona.

--- src/main/java/com/mycompany/iprofesores/IProfesores.java
+++ src/main/java/com/mycompany/iprofesores/IProfesores.java
@@ -3,6 +3,9 @@ package com.mycompany.iprofesores;
 
 
 public interface IProfesores {
-    public String agregarProfesor();
-    public boolean verificarCedula();
+    public boolean agregarProfesores(Profesor profesor);
+    public boolean actualizarProfesores(int i, Profesor profesor);
+    public void buscarProfesores(int id);
+    public boolean eliminarProfesores(int id);
+    public void imprimirProfesores();
 }
--- src/main/java/com/mycompany/persona/Persona.java
+++ src/main/java/com/mycompany/persona/Persona.java
@@ -16,6 +16,14 @@ public  class Persona {
         this.correroPersonal = correroPersonal;
         this.cedula = cedula;
     }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
     
 
 
